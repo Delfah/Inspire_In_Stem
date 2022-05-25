9914a0af398c8c262d51e2dd59525b8269bbd297
Delfah <kendidelfah@gmail.com>
2022-05-25 09:45:03 +0300

    assignment5.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 write a program to withdraw ksh 25000 if account balance is btween ksh 100,000 to 200,000
 30% if acc_bal ksh 500,000 and 1m
 above 1m deduct 15000
+
+
--- assignment4.py
+++ assignment4.py
@@ -1,13 +1,15 @@
 
 acc_bal = input("enter your account balance")
-acc_bal = 1000000
+acc_bal = 1000
 if (int(acc_bal)> 1000,000 and int (acc_bal) < 200,000) :
     acc_bal = acc_bal - 25,000
-    print("we have deducted ksh 25,000")
+    print("we have deducted ksh 25,000") 
+    print ("your account balance")
 
 elif (int(acc_bal)> 500,000 and int(acc_bal) < 200,000) :
-    acc_bal = acc_bal - float(0.3*acc_bal)
+    acc_bal = float (acc_bal) - (0.3*float (acc_bal))
     print("we have deducted 30 percent from your account")
+    print("account balance")
     
 elif (int(acc_bal) > 1000000):
     acc_bal = acc_bal - 15,000
