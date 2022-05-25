fbeb76eadeb1a465f7ad13dc0e13470cb4a9f3e1
Delfah <kendidelfah@gmail.com>
2022-05-25 09:40:22 +0300

    assignment5.py

---
+++ assignment5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# alist of vehicles
+
+vehicles = ['bmw','audi','toyota','mercedes','jeep']
+
+# using a for loop to print all vehicles
+
+for vehicle in vehicles :
+    print(vehicle)
+    if (vehicle == "jeep"):
+        print(vehicle.upper())
+    else:
+        print(vehicle.title())
+
+age = 30
+
+if age != 20:
+  print("you are not allowed")
+else:
+    print("you are allowed")
+
+
+    
