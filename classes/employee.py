ba1b69d36b61661ab7081e1fa7172d127e2a034c
Delfah <kendidelfah@gmail.com>
2022-06-02 12:22:29 +0300

    employee.py

--- classes/employee.py
+++ classes/employee.py
@@ -6,3 +6,15 @@
 # assignment
 
 ##################
+#create name and the employee salary
+
+class employee:
+    def __init__(self, _name, _salary, _ID ):
+        self.name = _name
+        self.salary = _salary
+        self.ID = _ID
+        
+    def company(self):
+        print("Hello,my name is" +self.name, "my salary is"  +self.salary,'in kenya shillings' "my ID number is" +self.ID)
+employee1 = employee("Mary", str(700000), str(7900))
+employee1.company()
