3a80e5dce8a38f5e90e3e8fef545461a86958dc9
Delfah <kendidelfah@gmail.com>
2022-07-06 11:34:38 +0300

    index.HTML

---
+++ WEB/js/index.js
@@ -0,0 +1,14 @@
+
+//singlenline convert in javascript
+/*
+multi.line comment in javascript
+*/
+
+let num1 = 134
+let num2 = 134
+console.log(num1 + num2)
+
+function submitbutton(){
+    alert("Button has been clicked")
+
+}
